4ed82e2701af4fd3ac0da8dd635a3a1cf13aa2c2
Adam Paterson <hello@adampaterson.co.uk>
2026-02-08 09:39:25 +0000

    feat: restructure git configuration to use lib.mkMerge for settings

--- src/modules/home/common/git/default.nix
+++ src/modules/home/common/git/default.nix
@@ -1,8 +1,19 @@
-{...}: {
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
   programs.git = {
     enable = true;
     settings.user.name = "Adam Paterson";
     settings.user.email = "hello@adampaterson.co.uk";
     settings.user.signingKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL1CJVWAx5tlEl1onIshZURohd68JMza5uk1E+eStOUn";
+    settings = lib.mkMerge [
+      (lib.mkIf pkgs.stdenv.isDarwin {
+        gpg.format = "ssh";
+        gpg.ssh.program = "${config.home.homeDirectory}/.local/bin/op-ssh-sign";
+      })
+    ];
   };
 }
