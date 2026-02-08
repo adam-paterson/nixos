8806d4d448a6ae22ba5a8121adb002f5a7698101
Adam Paterson <hello@adampaterson.co.uk>
2026-02-08 09:22:44 +0000

    Update git config with correct email and signing key

--- src/modules/home/common/git/default.nix
+++ src/modules/home/common/git/default.nix
@@ -2,6 +2,7 @@
   programs.git = {
     enable = true;
     settings.user.name = "Adam Paterson";
-    settings.user.email = "adam@example.com";
+    settings.user.email = "hello@adampaterson.co.uk";
+    settings.user.signingKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL1CJVWAx5tlEl1onIshZURohd68JMza5uk1E+eStOUn";
   };
 }
